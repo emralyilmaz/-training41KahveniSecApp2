b7d61a0403afc9ed76dce4937549e9267b8fec96
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-07 16:55:39 +0300

    Sign Out Butonunu Oluşturma

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,8 +8,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home"),
-    );
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(252, 251, 227, 1),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(149, 87, 48, 1),
+          title: Text(
+            "Anasayfa",
+            style: TextStyle(color: Color.fromRGBO(252, 251, 227, 1)),
+          ),
+          actions: [
+            FlatButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.person_outline,
+                    color: Color.fromRGBO(252, 251, 227, 1)),
+                label: Text("Çıkış",
+                    style: TextStyle(color: Color.fromRGBO(252, 251, 227, 1))))
+          ],
+        ));
   }
 }
